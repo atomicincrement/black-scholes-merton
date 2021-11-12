4d1815bdd1691bd7c151dedb9b0d35401964ab94
andy-thomason <andy@atomicinrement.com>
2021-11-12 16:37:52 +0000

    Add qnorm prior to parallelisation

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # black-scholes-merton
 
+## Initial investigation
+
+This shows how a naive speed improvement can be achieved using a compiled
+language:
+
 Use rustup https://rustup.rs/ to install cargo and rustc.
 
+```
 $ time python python/bs.py
 Call option price with Monte Carlo approach:  1.2405654541052522
 Put option price with Monte Carlo approach:  1.5519545170110747
@@ -19,3 +25,35 @@ put option price  = 1.5523784672155365
 real    0m3.230s
 user    0m3.217s
 sys     0m0.013s
+```
+
+## How to improve this.
+
+### Step 1 is to simplify the function.
+
+The function we evaluate can be reduced to an exponential
+of a linear function:
+
+```
+        let k1 = f64::ln(underlying_price) + t * (rf - s2);
+        let k2 = sigma * sqrt_t;
+        move |rand| f64::exp(k1 + k2 * rand)
+```
+
+```
+$ time target/release/black-scholes-montecarlo 
+call option price = 1.2409853902861983
+put option price  = 1.5525796331857264
+
+real    0m2.992s
+user    0m2.980s
+sys     0m0.012s
+```
+
+The time is roughly the same (within the noise) showing that
+the random number generator is the main source of pain.
+
+
+### Step 2 is to replace the random number generator with a hash.
+
+Replacing the RNG with a hash enables us to generate 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,103 @@
 use rand::distributions::Distribution;
 use statrs::distribution::Normal;
+use rayon::prelude::*;
+
+mod generated {
+    type fty = f64;
+    type ity = i64;
+    type uty = u64;
+    
+    use std::f64::consts::LOG2_10;
+    use std::f64::consts::LOG2_E;
+    use std::f64::consts::PI;
+    
+    fn select(a: bool, b: fty, c: fty) -> fty {
+        if a {
+            b
+        } else {
+            c
+        }
+    }
+    
+    fn iabs(i: ity) -> ity {
+        i.abs()
+    }
+    
+    fn f(f: fty) -> fty {
+        f
+    }
+    
+    fn from_bits(u: uty) -> fty {
+        fty::from_bits(u)
+    }
+    
+    fn to_bits(f: fty) -> uty {
+        fty::to_bits(f)
+    }
+
+    fn exp2(arg: fty) -> fty {
+        let r: fty = arg.round();
+        let mul: fty = fty::from_bits(
+            (r.mul_add(0x0010000000000000_u64 as fty, 0x3ff0000000000000_u64 as fty)) as uty,
+        );
+        let x: fty = arg - r;
+        (from_bits(4549839347750377909u64))
+            .mul_add(x, from_bits(4563827094295188139u64))
+            .mul_add(x, from_bits(4576698039041613846u64))
+            .mul_add(x, from_bits(4588159642448921967u64))
+            .mul_add(x, from_bits(4597823092488205992u64))
+            .mul_add(x, from_bits(4604418534717280147u64))
+            .mul_add(x, from_bits(4607182418800017408u64))
+            * mul
+    }
+
+    fn exp(arg: fty) -> fty {
+        exp2(arg * LOG2_E)
+    }
+
+    fn negate_on_odd(x: fty, y: fty) -> fty {
+        let sign_bit: uty = (((x as ity) & 1) << 63i32) as uty;
+        fty::from_bits(sign_bit ^ y.to_bits())
+    }
+    fn recip_approx(x: fty) -> fty {
+        let y: fty = fty::from_bits(
+            ((x.abs().to_bits() as fty).mul_add(-1.0, 0x3ff0000000000000_u64 as fty * 2.0)) as uty,
+        );
+        (y - 0.08).copysign(x)
+    }
+    fn sqrt_approx(x: fty) -> fty {
+        let y: fty = fty::from_bits(
+            ((x.abs().to_bits() as fty).mul_add(0.5, 0x3ff0000000000000_u64 as fty * 0.5)) as uty,
+        );
+        y - 0.08
+    }
+    fn cbrt_approx(x: fty) -> fty {
+        let y: fty = fty::from_bits(
+            ((x.abs().to_bits() as fty).mul_add(1.0 / 3.0, 0x3ff0000000000000_u64 as fty * 2.0 / 3.0))
+                as uty,
+        );
+        (y - 0.08).copysign(x)
+    }
+
+    pub fn qnorm(arg: fty) -> fty {
+        let x: fty = arg - 0.5;
+        let recip: fty = 1.0 / (x * x - 0.25);
+        let y: fty = (from_bits(4730221388428958202u64))
+            .mul_add(x * x, -from_bits(4731626383781768040u64))
+            .mul_add(x * x, from_bits(4727627778628654481u64))
+            .mul_add(x * x, -from_bits(4720012863723153492u64))
+            .mul_add(x * x, from_bits(4708869911609092829u64))
+            .mul_add(x * x, -from_bits(4695087533321972728u64))
+            .mul_add(x * x, from_bits(4678670384600451257u64))
+            .mul_add(x * x, -from_bits(4658680898319303328u64))
+            .mul_add(x * x, from_bits(4635605149421499302u64))
+            .mul_add(x * x, from_bits(4578476110820645018u64))
+            .mul_add(x * x, from_bits(4611041379213747643u64))
+            .mul_add(x * x, -from_bits(4603819697584151827u64))
+            * x;
+        y * recip
+    }
+}
 
 struct BlackScholes {
     underlying_price : f64,
@@ -16,19 +114,37 @@ enum OptionType {
 }
 
 impl BlackScholes {
-    // Naive call option price calculation.
-    fn option_price(&self, ot: OptionType) -> f64 {
+    fn naive_stock_price_function(&self) -> impl Fn(f64) -> f64 {
         let sigma = self.volatility_of_the_underlying;
         let rf = self.risk_free_interest_rate;
         let t = self.time_to_maturity;
-        let up = self.underlying_price;
+        let underlying_price = self.underlying_price;
         let s2 = 0.5 * f64::powi(sigma, 2);
-        let root_t = f64::sqrt(t);
+        let sqrt_t = f64::sqrt(t);
 
+        move |rand| underlying_price * f64::exp(t * (rf - s2) + sigma * sqrt_t * rand)
+    }
+
+    fn improved_stock_price_function(&self) -> impl Fn(f64) -> f64 {
+        let sigma = self.volatility_of_the_underlying;
+        let rf = self.risk_free_interest_rate;
+        let t = self.time_to_maturity;
+        let underlying_price = self.underlying_price;
+        let s2 = 0.5 * f64::powi(sigma, 2);
+        let sqrt_t = f64::sqrt(t);
+
+        // Reduce the function to its bare bones.
+        let k1 = f64::ln(underlying_price) + t * (rf - s2);
+        let k2 = sigma * sqrt_t;
+        move |rand| f64::exp(k1 + k2 * rand)
+    }
+
+    // Naive call option price calculation.
+    fn naive_option_price(&self, ot: OptionType) -> f64 {
         let mut r = rand::thread_rng();
         let n = Normal::new(0.0, 1.0).unwrap();
 
-        let stock_price = |rand| up * f64::exp(t * (rf - s2) + sigma * root_t * rand);
+        let stock_price = self.naive_stock_price_function();
 
         use OptionType::*;
         let average = match ot {
@@ -40,6 +156,30 @@ impl BlackScholes {
             }).sum::<f64>() / (self.number_of_iterations as f64),
         };
 
+        let rf = self.risk_free_interest_rate;
+        let t = self.time_to_maturity;
+        f64::exp(-1.0 * rf * t) * average
+    }
+
+    // Naive call option price calculation.
+    fn improved_option_price(&self, ot: OptionType) -> f64 {
+        let mut r = rand::thread_rng();
+        let n = Normal::new(0.0, 1.0).unwrap();
+
+        let stock_price = self.improved_stock_price_function();
+
+        use OptionType::*;
+        let average = match ot {
+            Call => (0..self.number_of_iterations).map(|_| {
+                f64::max(stock_price(n.sample(&mut r)) - self.strike_price, 0.0)
+            }).sum::<f64>() / (self.number_of_iterations as f64),
+            Put => (0..self.number_of_iterations).map(|_| {
+                f64::max(self.strike_price - stock_price(n.sample(&mut r)), 0.0)
+            }).sum::<f64>() / (self.number_of_iterations as f64),
+        };
+
+        let rf = self.risk_free_interest_rate;
+        let t = self.time_to_maturity;
         f64::exp(-1.0 * rf * t) * average
     }
 }
@@ -54,6 +194,22 @@ fn main() {
         number_of_iterations : 100000000,
     };
 
-    println!("call option price = {}", bs.option_price(OptionType::Call));
-    println!("put option price  = {}", bs.option_price(OptionType::Put));
+    for i in 0..100 {
+        let x = i as f64 * (1.0/100.0);
+        println!("{:12.8} {:24.20}", x, generated::qnorm(x));
+    }
+
+    let step = 1;
+
+    match step {
+        0 => {
+            println!("call option price = {}", bs.naive_option_price(OptionType::Call));
+            println!("put option price  = {}", bs.naive_option_price(OptionType::Put));
+        }
+        1 => {
+            println!("call option price = {}", bs.improved_option_price(OptionType::Call));
+            println!("put option price  = {}", bs.improved_option_price(OptionType::Put));
+        }
+        _ => unreachable!(),
+    }
 }
